e5fec12ac60ecbbacddf3d9526f79a19015cab47
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2023-04-22 12:06:14 -0500

    Refactor structure.

--- backend/kotlin/world-region-services-kt/src/main/kotlin/com/themusketeers/world/region/services/Application.kt
+++ backend/kotlin/world-region-services-kt/src/main/kotlin/com/themusketeers/world/region/services/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WorldRegionServicesApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<WorldRegionServicesApplication>(*args)
+    runApplication<Application>(*args)
 }
