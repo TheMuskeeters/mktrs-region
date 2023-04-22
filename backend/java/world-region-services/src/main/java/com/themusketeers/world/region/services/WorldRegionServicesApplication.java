5879cc0827c62fea98b1abce38cc7a54bca873f6
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2023-04-21 20:51:42 -0500

    Initial commit.

---
+++ backend/java/world-region-services/src/main/java/com/themusketeers/world/region/services/WorldRegionServicesApplication.java
@@ -0,0 +1,13 @@
+package com.themusketeers.world.region.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldRegionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldRegionServicesApplication.class, args);
+	}
+
+}
---
+++ backend/kotlin/world-region-services/src/main/kotlin/com/themusketeers/world/region/services/WorldRegionServicesApplication.kt
@@ -0,0 +1,11 @@
+package com.themusketeers.world.region.services
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorldRegionServicesApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorldRegionServicesApplication>(*args)
+}
