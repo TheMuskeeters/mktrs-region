dc117854d66d4c9d1f5b64a29f65b1967f2e7c7f
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2023-04-22 13:08:47 -0500

    Set javadoc/Kdoc in projects.

--- backend/kotlin/world-region-services-kt/src/main/kotlin/com/themusketeers/world/region/services/Application.kt
+++ backend/kotlin/world-region-services-kt/src/main/kotlin/com/themusketeers/world/region/services/Application.kt
@@ -1,11 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   APPLICATION.KT                                              */
+/* Copyright (c), 2023 TheMuskeeters                                          */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ Apr.22/2023  COQ  File created.
+ -----------------------------------------------------------------------------*/
 package com.themusketeers.world.region.services
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * Entry point for running the application.
+ */
 @SpringBootApplication
 class Application
 
+/**
+ * Running application definition entry point.
+ *
+ * @param args Includes the command line parameters for the application.
+ */
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
--- backend/kotlin/world-region-services-kt/src/test/kotlin/com/themusketeers/world/region/services/ApplicationTests.kt
+++ backend/kotlin/world-region-services-kt/src/test/kotlin/com/themusketeers/world/region/services/ApplicationTests.kt
@@ -1,12 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   APPLICATIONTESTS.KT                                         */
+/* Copyright (c), 2023 TheMuskeeters                                          */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ Apr.22/2023  COQ  File created.
+ -----------------------------------------------------------------------------*/
 package com.themusketeers.world.region.services
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+/**
+ * Unit tests for Application class.
+ */
 @SpringBootTest
 class ApplicationTests {
 
+    /**
+     * Validates the Spring Boot context is loaded.
+     */
     @Test
+    @DisplayName("Verify context loads.")
     fun contextLoads() {
     }
 
